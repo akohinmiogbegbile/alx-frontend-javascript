aa175545a8adb20d9c2b61b46606b5071b5c1d78
akohinmiogbegbile <akohinmi90@gmail.com>
2024-03-04 22:56:50 +0000

    ALX FrontEnd 1

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments(empList) {
+        return Object.keys(empList).length;
+      },
+    };
+  }
+  
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,2 @@
+ALX Front End Development
+0x00. ES6 Basics
