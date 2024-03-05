cbe33bb17d9dfc3a9acb9052f752b2f0160e6cfe
akohinmiogbegbile <akohinmi90@gmail.com>
2024-03-05 00:28:47 +0000

    alx

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -13,4 +13,6 @@ export function taskFirst() {
   
     return combination;
   }
+
+
   
--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,5 @@
 import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
+
+
